c9fa3dc2f34928448efb0b8dcf2f48ab96f41e37
pixelunknown <lancellottimattia@gmail.com>
2021-10-19 20:37:58 +0200

    Add: decimal->binary conversion, string-reverse to reverse a string

--- main.rkt
+++ main.rkt
@@ -34,9 +34,29 @@
                        (opposite (substring binary 0 1))
                        (complement-one (substring binary 1))))))
 
+;;; Recursive decimal to binary translation
+;;; This function will recursively translate any positive decimal number to its
+;;; binary counterpart.
+(define decimal->binary
+  (lambda (decimal) (if (> decimal 1)
+                      ;; Using the multiple divisions method to find the binary
+                      ;; version of a number.
+                      (string-append
+                        (number->string (remainder decimal 2))
+                        (decimal->binary (quotient decimal 2))) 
+                      "1")))
+
+;;; This is an helper function that takes a string as input an will output it
+;;; reversed.
+(define string-reverse
+  (lambda (str) (list->string (reverse (string->list str)))))
+
 ;;; Tests
 (printf "1: complement 1 of the number ~a: ~s~%"
         inet-ip (complement-one inet-ip))
 
 (printf "2: signed notation of the number ~a: ~s~%"
         rand-b (signed-notation rand-b))
+
+(printf "3: converting the decimal number ~a to binary: ~s~%"
+        "157" (string-reverse (decimal->binary 157)))
