d42232e2f303b912f2461733bbc8938f4ba86a5f
pixelunknown <lancellottimattia@gmail.com>
2021-10-21 22:35:29 +0200

    Cleaner code

--- main.rkt
+++ main.rkt
@@ -5,24 +5,28 @@
 
 ;;; 0 <-> 1
 (define opposite
-  (lambda (digit) (if (string=? digit "1") "0" "1")))
+  (lambda (digit) (cond
+                    [(string=? digit "1") "0"]
+                    [else "1"])))
+
+(define one?
+  (lambda (number) (= number 1)))
 
 ;;; This function will just swap the first bit of the given binary string as the
 ;;; signed notation says.
 (define signed-notation
-  (lambda (binary) (let ((len (string-length binary)))
+  (lambda (binary) (let ([len (string-length binary)])
                      ;; This will take the first bit and change it to the
                      ;; opposite to make a negative/positive number.
                      (string-append
                        (opposite (substring binary 0 1))
                        (substring binary 1)))))
 
-
 ;;; Recursive 1's complement 
 ;;; This function will recursively iterate through the given string of digits
 ;;; until it gets to a single digit to then return the opposite.
 (define complement-one
-  (lambda (binary) (if (= (string-length binary) 1)
+  (lambda (binary) (if (one? (string-length binary))
                      ;; If the passed binary number is composed by just one
                      ;; digit there is no need to do further operation other
                      ;; than switching this single digit.
@@ -38,26 +42,27 @@
 ;;; This function will recursively translate any positive decimal number to its
 ;;; binary counterpart.
 (define decimal->binary
-  (lambda (decimal) (if (> decimal 1)
+  (lambda (decimal) (cond
+                      [(> decimal 1)
                       ;; Using the multiple divisions method to find the binary
                       ;; version of a number.
                       (string-append
                         (number->string (remainder decimal 2))
-                        (decimal->binary (quotient decimal 2))) 
-                      "1")))
+                        (decimal->binary (quotient decimal 2)))]
+                      [else "1"])))
 
 ;;; Recursive binary to decimal translation
 ;;; This function accepts a binary number as a string and will convert it to
 ;;; decimal.
 (define binary->decimal
-  (lambda (binary) (let ((len (string-length binary))
-                         (dgt (substring binary 0 1)))
-                     (if (zero? len)
-                       (string->number (substring binary 0 1))
-                       (if (string=? dgt "1")
+  (lambda (binary) (let ([len (string-length binary)]
+                         [dgt (substring binary 0 1)])
+                     (cond
+                       [(zero? len) (string->number (substring binary 0 1))]
+                       [(string=? dgt "1")
                          (+ (expt 2 (sub1 len))
-                            (binary->decimal (substring binary 1)))
-                         0)))))
+                            (binary->decimal (substring binary 1)))]
+                       [else 0]))))
 
 ;;; This is an helper function that takes a string as input an will output it
 ;;; reversed.
@@ -78,4 +83,4 @@
         inet-ip (binary->decimal inet-ip))
 
 (printf "5: converting the decimal number ~a to binary ~s~%"
-        "192" (string-reverse (decimal->binary 192)))
+        192 (string-reverse (decimal->binary 192)))
