3bcde776db85e5c97aed4c1de4f8b2ca8e6e8e06
pixelunknown <lancellottimattia@gmail.com>
2021-10-24 19:29:10 +0200

    Add: padding to add 0s before the number

--- bits.rkt
+++ bits.rkt
@@ -1,5 +1,5 @@
 (module bits racket
-  (provide one? opposite)
+  (provide one? opposite padding)
 
   ;;; This function checks if the given number is 1 or not
   (define one?
@@ -9,4 +9,13 @@
   (define opposite
     (lambda (digit) (cond
                       [(string=? digit "1") "0"]
-                      [else "1"]))))
+                      [else "1"])))
+  (define padding
+    (lambda (bits) (let* ([len (length bits)]
+                          [pow (exact-ceiling (log len 2))]
+                          [pad (- (expt 2 pow) len)])
+                     (cond
+                       [(not (zero? pad))
+                        (append
+                          (build-list pad (lambda (x) 0)) bits)]
+                       [else bits])))))
