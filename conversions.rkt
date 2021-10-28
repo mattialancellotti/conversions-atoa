d1b26f73acbd27056bf15df39dd98c559c9b6626
pixelunknown <lancellottimattia@gmail.com>
2021-10-28 14:17:28 +0200

    Add: comments to explain what the hell are those parenthesis

--- conversions.rkt
+++ conversions.rkt
@@ -1,11 +1,10 @@
 (module conversions racket/base
   (require racket/list)
-  (require "notations.rkt")
-  (require "bits.rkt")
+  (require "notations.rkt" "bits.rkt")
 
   (provide base10->baseN baseN->base10)
 
-  ;;; Recursive decimal to N base transaltion
+  ;;; Recursive decimal to N base translation
   ;;; This funciton takes a decimal number and the base to translate that number
   ;;; to. Will return a list of ordered character composing the new
   ;;; rapresentation.
@@ -18,16 +17,26 @@
                             [(negative? number)
                              (signed-notation
                                (padding (base10->baseN (abs number) base)))]
+                            ;; This piece of code will recursively divide the
+                            ;; given number until it gets to 0. After all the
+                            ;; created functions have returned the result of the
+                            ;; first instance will be a list of numbers, each of
+                            ;; them a digit of that same number rapresented
+                            ;; using a different base.
                             [else
                               (append
                                 (base10->baseN (quotient number base) base)
                                 (list (remainder number base)))])))
 
+  ;;; Recursive N base to decimal translation
+  ;;; This function takes a list and a base as input used to translate the given
+  ;;; number to its decimal counterpart.
   (define baseN->base10
     (lambda (number base) (let ([len (length number)])
                             (cond
                               [(empty? number) 0]
                               [else
+                                ;; Using the multiplications method.
                                 (+
                                   (* (first number) (expt base (sub1 len)))
                                   (baseN->base10
