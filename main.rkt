f9aea8bab0ec6cd1b368c8e7ed8cf29fc1b14ff4
pixelunknown <lancellottimattia@gmail.com>
2021-11-05 19:03:51 +0100

    Add: display-complement

--- main.rkt
+++ main.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
+(require racket/list racket/string)
 (require "bits.rkt" "notations.rkt" "conversions.rkt")
 
 (define inet-ip '(1 1 0 0 0 0 0 0))
@@ -11,6 +12,12 @@
       (printf ":: signed notation of the number ~a: ~s~%"
               (build-number number) (complement-one number)))))
 
+(define display-complement
+  (lambda (number func)
+    (when (list? number)
+      (printf ":: complement of the number ~a: ~s~%"
+              (build-number number) (func number)))))
+
 (define display-conversion
   (lambda (number baseStart baseEnd)
     (when (number? number)
@@ -20,8 +27,8 @@
 ;;; Tests
 (display-signed-notation inet-ip)
 (display-signed-notation  rand-b)
+(display-complement  rand-b complement-two)
+(display-complement inet-ip complement-one)
 (display-conversion  50 10  4)
 (display-conversion 307 10 16)
 (display-conversion 389 10 20)
-
-(complement-two rand-b)
