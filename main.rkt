f512415f9b1f0f5ebc2bc0044665eb3e270babf5
pixelunknown <lancellottimattia@gmail.com>
2021-12-16 10:05:06 +0100

    New project layout

--- main.rkt
+++ main.rkt
@@ -1,29 +1,11 @@
 #lang racket/base
 
 (require racket/list racket/string)
-(require "bits.rkt" "notations.rkt" "conversions.rkt")
+(require "src/display.rkt" "src/notations.rkt")
 
 (define inet-ip '(1 1 0 0 0 0 0 0))
 (define rand-b  '(1 0 1 1 0 1 1 1))
 
-(define display-signed-notation
-  (lambda (number)
-    (when (list? number)
-      (printf ":: signed notation of the number ~a: ~s~%"
-              (build-number number) (complement-one number)))))
-
-(define display-complement
-  (lambda (number func)
-    (when (list? number)
-      (printf ":: complement of the number ~a: ~s~%"
-              (build-number number) (func number)))))
-
-(define display-conversion
-  (lambda (number baseStart baseEnd)
-    (when (number? number)
-      (printf ":: conversion of number ~a from base ~a to ~a: ~s~%"
-              number baseStart baseEnd
-              (convert number baseStart baseEnd)))))
 ;;; Tests
 (display-signed-notation inet-ip)
 (display-signed-notation  rand-b)
---
+++ src/display.rkt
@@ -0,0 +1,25 @@
+#lang racket/base
+
+(require racket/list racket/string)
+(require "bits.rkt" "notations.rkt" "conversions.rkt")
+
+(provide display-signed-notation display-complement display-conversion)
+
+(define display-signed-notation
+  (lambda (number)
+    (when (list? number)
+      (printf ":: signed notation of the number ~a: ~s~%"
+              (build-number number) (complement-one number)))))
+
+(define display-complement
+  (lambda (number func)
+    (when (list? number)
+      (printf ":: complement of the number ~a: ~s~%"
+              (build-number number) (func number)))))
+
+(define display-conversion
+  (lambda (number baseStart baseEnd)
+    (when (number? number)
+      (printf ":: conversion of number ~a from base ~a to ~a: ~s~%"
+              number baseStart baseEnd
+              (convert number baseStart baseEnd)))))
